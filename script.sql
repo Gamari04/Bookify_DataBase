baede4e561e0bbf0575e16ba3ee4ae69e069e3f4
Salma <salmaelgmiri04@gmail.com>
2023-11-20 11:03:12 +0000

    lister les reservations et les employées avec des conditions

--- script.sql
+++ script.sql
@@ -33,28 +33,46 @@ CREATE TABLE Reservation (
 
 INSERT INTO Departement (id, Nom)
 VALUES
-(1,'Comptabilite');
-(2,'Finance');
+(1,'Comptabilite'),
+(2,'Finance'),
+(3,'resourses humaines'),
+(4,'Informatique'),
+(5,'Physique'),
+(6,'Chimique');
 
 INSERT INTO Equipement (id, Nom)
 VALUES
 (1,'des ordinateurs');
 (2,'des tableaux');
+(3,'des bureaux');
 
 INSERT INTO Employe (id, Nom, Email, Departement_id)
 VALUES
-(1,'Salma','salmaelgmiri04@gmail.com',1);
-(2,'Omar','omarelgmiri04@gmail.com',1);
+(1,'Salma','salmaelgmiri04@gmail.com',1),
+(2,'Omar','omarelgmiri04@gmail.com',2),
+(3,'othmane','othmaneelgmiri04@gmail.com',3),
+(4,'Amir','Amirelgmiri04@gmail.com',3),
+(5,'Basma','Basmaelgmiri04@gmail.com',3),
+(6,'Karim','Karimelgmiri04@gmail.com',3);
 
 INSERT INTO Salle (id, Nom, Capacite, Equipement_id)
 VALUES
-(1,'reunion',10,1)
-(2,'debrifing',10,1)
+(1,'reunion',10,1),
+(2,'debrifing',20,3),
+(3,'Break',5,2);
+
 
 INSERT INTO Reservation (id, Salle_id, Employe_id, Date_Debut, Date_Fin)
 VALUES
-(1, 1, 1, 2023-11-19 13:00:00, 2023-11-19 15:00:00),
-(2, 2, 2, 2023-11-20 16:00:00, 2023-11-20 18:00:00),
-(3, 3, 3, 2023-11-23 12:00:00, 2023-11-23 16:00:00);
+(1, 1, 1, '2023-11-19 13:00:00', '2023-11-19 15:00:00'),
+(2, 2, 2, '2023-11-20 16:00:00', '2023-11-20 18:00:00'),
+(3, 3, 3, '2023-11-23 12:00:00', '2023-11-23 16:00:00');
 
+SELECT Nom FROM Employe ORDER BY Nom;
+
+
+SELECT Reservation.id AS "Reservation", Salle.Nom AS "Nom de la salle", Employe.Nom AS "Nom de l'employe"
+FROM Reservation
+JOIN Salle ON Reservation.Salle_id = Salle.id
+JOIN Employe ON Reservation.Employe_id = Employe.id;
 
